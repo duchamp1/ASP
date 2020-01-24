d2ed3b8fd81c62a1cf649ef9ffeaeebab462c1c3
mondr <mondr@PC-ISHIZAKI>
2020-01-24 16:53:21 +0900

    プロジェクト ファイルを追加します。

---
+++ WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="WebForm1.aspx.cs" Inherits="WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>ファイルアップロード</title>
+</head>
+<body>
+    <form id="Form1" method="post" runat="server" enctype="multipart/form-data">
+    <p>送信するファイルを指定して、［送信］ボタンを押してください。</p>
+    <p>
+        <asp:FileUpload ID="FileUpload1" runat="server" Width="703px" />
+        </p>
+    <p><asp:Button id="btnUpload" runat="server" Text="送信" OnClick="btnUpload_Click" Width="50px" />
+       <asp:Button id="btnEnd" runat="server" Text="終了" OnClick="btnEnd_Click" Width="50px" /></p>
+        <p>
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </p>
+    
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿//アップロードされたファイルを簡単に保存する方法
+//http://aspnet.keicode.com/controls/fileupload-save-file.php
+//20200124 アップロードによりタイムスタンプが更新されてしまうため、ファイルコピー処理に変更 
+//         「終了」ボタン追加 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WebForm1 : System.Web.UI.Page
+{
+    //20180629 UPDATE START
+    const string SAVEPATH = "C:\\UploadedFiles";
+    const string MSG = "The uploaded files was saved as ";
+    //20180629 UPDATE END
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            //アップロード先
+            string path = SAVEPATH;
+            //string path = "C:\\UploadedFiles";
+            //ファイル名の取得
+            string oldFileName = FileUpload1.PostedFile.FileName; //20200124 ADD 
+            string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+            //パスとファイル名の連結
+            string filePath = Path.Combine(path, fileName);
+            //20200124 UPDATE START 
+            //FileUpload1.SaveAs(filePath);
+            File.Copy(oldFileName, filePath);//ファイルコピー
+            //20200124 UPDATE END
+            Label1.Text = MSG + filePath;
+//            Label1.Text = "The uploaded files was saved as " + filePath;
+        }
+    }
+
+    //20200124 ADD 
+    protected void btnEnd_Click(object sender, EventArgs e)
+    {
+        Response.Write("<script language='javascript'> { window.close();}</script>");
+    }
+}
